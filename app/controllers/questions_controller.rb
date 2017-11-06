c4f65fc591772e9e4be5a37bfb858294581a1041
Cecile Alfred <cecile.alfredx@gmail.com>
2017-11-06 14:07:44 +0000

    Added comments on the Controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,10 @@
 class QuestionsController < ApplicationController
   def answer
-    # instance var will make it accessible in the view
+    # TODO: Display user's question and the coach's answer
+    # instance @var will make it accessible in the view
+    # 1. Get the question from the form (name of the input)
     @question = params[:user_question]
+    # 2. Get the answer (String) from the conditional
     @answer = coach_answer(@question)
   end
 
@@ -11,10 +14,13 @@ class QuestionsController < ApplicationController
 
   def coach_answer(your_message)
     # TODO: return coach answer to your_message
+    # 1. Is your_message a question (includes "?")
     if your_message.include? "?"
       return "Silly question, get dressed and go to work!"
+      # 2. Is your_message a specific String
     elsif your_message == "I am going to work right now!"
       return ""
+      # 3. Is your_message not a question OR specific String
     else
       return "I don't care, get dressed and go to work!"
     end
